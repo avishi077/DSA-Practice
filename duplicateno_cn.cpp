252b3f65548d90fa783ec07793e6b280c4232b88
avishi07 <avishisharma776@gmail.com>
2025-12-14 21:41:01 +0530

    intersection_cn

--- duplicateno_cn.cpp
+++ duplicateno_cn.cpp
@@ -1,6 +1,5 @@
 #include<algorithm>
-int findDuplicate(vector<int> &arr)
-{
+int findDuplicate(vector<int> &arr){
   sort(arr.begin(),arr.end());
   for(int i=0;i<arr.size()-1;++i){
     if (arr[i] == arr[i + 1]) {
