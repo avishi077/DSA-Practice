d0c57c2fb40925d28c81ee03acee8279cd7434b4
avishi077 <151861975+avishi077@users.noreply.github.com>
2026-07-15 23:07:18 +0530

    [169] [Time Beats: 100.00%] [Memory Beats: 68.61%] - LeetPush

---
+++ 169-Majority-Element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt=0;
+        int el=0;
+        for(int i=0;i<nums.size();i++){
+            if (cnt==0){
+                el=nums[i];
+                cnt=1;
+            }
+            else if (el==nums[i]){
+                cnt++;
+            }
+
+            else{
+                cnt--;
+            }
+        }
+        return el;
+    }
+};
