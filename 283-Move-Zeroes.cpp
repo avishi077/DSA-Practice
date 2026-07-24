f6bfaaadc48de37fc4bef96d366f6a1ad6451c1e
avishi077 <151861975+avishi077@users.noreply.github.com>
2026-07-24 23:51:57 +0530

    [283] [Time Beats: 100.00%] [Memory Beats: 56.46%] - LeetPush

---
+++ 283-Move-Zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos=0;
+        int n=nums.size();
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=0){
+                nums[pos]=nums[i];
+                pos++;            
+            }
+        }
+        for(int i=pos; i<n; i++){
+            nums[i]=0;
+        }
+        
+    }
+};
