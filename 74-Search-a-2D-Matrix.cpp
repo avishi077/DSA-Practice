12e485478662c61a7cdfabd222c22e5d5cfbdc7d
avishi077 <151861975+avishi077@users.noreply.github.com>
2026-07-25 23:40:35 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 98.88%] - LeetPush

---
+++ 74-Search-a-2D-Matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int low=0;
+        int high=(m*n)-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int rowInd=mid/n;
+            int colInd=mid%n;
+            if(matrix[rowInd][colInd]==target) return true;
+            else if(matrix[rowInd][colInd]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
