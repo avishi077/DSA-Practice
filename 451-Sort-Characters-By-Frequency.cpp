8498c52ede0a77b58633f5778cf71cc359539f77
avishi077 <151861975+avishi077@users.noreply.github.com>
2026-07-31 20:24:29 +0530

    [451] [Time Beats: 100.00%] [Memory Beats: 40.36%] - LeetPush

---
+++ 451-Sort-Characters-By-Frequency.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        for(char c:s){
+            mp[c]++;
+        }
+        vector<pair<char,int>> v(mp.begin(),mp.end());
+        sort(v.begin(), v.end(), [](auto &a, auto &b){
+            return a.second>b.second;
+        });
+        string ans="";
+        for(int i=0;i<v.size();i++){
+            ans.append(v[i].second, v[i].first);
+        }
+        return ans;
+    }
+};
